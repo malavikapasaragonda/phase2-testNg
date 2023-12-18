7bbbf4bc7778f2a8bdc46ef9cbf2bb322be46338
malavika <malavika.rathnakar@gmail.com>
2023-12-18 21:08:55 +0530

    added files

---
+++ src/test/java/com/simplilearn/testng/amazon/AmazonLinkVerificationTest.java
@@ -0,0 +1,5 @@
+package com.simplilearn.testng.amazon;
+
+public class AmazonLinkVerificationTest {
+
+}
---
+++ src/test/java/com/simplilearn/testng/group/BrowserBasedGroupTest.java
@@ -0,0 +1,5 @@
+package com.simplilearn.testng.group;
+
+public class BrowserBasedGroupTest {
+
+}
